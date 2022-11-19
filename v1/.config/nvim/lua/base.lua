07564ded0739dbbdd5edc9989c46fcfa80b343c1
Joel D'Autilio <joeldautilio@gmail.com>
2022-11-19 15:42:48 -0300

    20221119

---
+++ v1/.config/nvim/lua/base.lua
@@ -0,0 +1,16 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = false
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
--- v1/.config/polybar/launch.sh
+++ v1/.config/polybar/launch.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Kill running instances
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+# polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
 killall -q polybar
 
-# While until the processes have been killed
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+# Launch mybar
+echo "---" | tee -a /tmp/polybar.log
+polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
-polybar --reload xmonad-mainbar -c ~/.config/polybar/config.ini
+echo "Bar launched..."
